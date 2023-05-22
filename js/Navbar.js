60d99a0be58ecfa6462f7cd7566c417937cdfe97
Gonzalo Jimenez <gonza.99dj@gmail.com>
2023-05-22 15:04:29 -0300

    actualizar ramas

--- js/Navbar.js
+++ js/Navbar.js
@@ -1,121 +1,28 @@
-  /* ----------------------------------
-     START #page-header js rules
-   ---------------------------------- */
-
-  /* *******  start open/close navbar search box   ********/
-  $(".header-search-box .close-search , .header-search-box").on("click", () => {
-    $(".header-search-box").removeClass("show");
-  });
-
-  /* Start bootstrap Scrollspy Options  */
-  //on one page demos only
-  if (navMain) {
-    $(bdyOnePage).scrollspy({
-      target: navMain,
-      offset: navMain.innerHeight() + 1,
-    });
+var open = false;
+document.getElementById("burgermenu").addEventListener('click', function(e) {
+  var nr1 = document.getElementById("nr1");
+  var nr2 = document.getElementById("nr2");
+  var nr3 = document.getElementById("nr3");
+  var menu = document.getElementById("menu");
+  if(open === true) {
+    open = false;
+    menu.style.transform = "translateX(100%)";
+    nr1.style.rotate = "0deg";
+    nr1.style.transform = "translate3D(0px,0px,0)";
+    nr1.style.width = "50px";
+    nr2.style.opacity = "1";
+    nr3.style.rotate = "0deg";
+    nr3.style.transform = "translate3D(0px,0px,0)";
+    nr3.style.width = "50px";
+  } else {
+    open = true;
+    menu.style.transform = "translateX(0%)";
+    nr1.style.rotate = "-45deg";
+    nr1.style.transform = "translate3D(-8px,14px,0)";
+    nr1.style.width = "55px";
+    nr2.style.opacity = "0";
+    nr3.style.rotate = "45deg";
+    nr3.style.transform = "translate3D(-8px,-15px,0)";
+    nr3.style.width = "55px";
   }
-
-  // make the menu stick on top whlie reloading
-  if ($(this).scrollTop() > 50) {
-    if (!$(pageHeader).hasClass("is-sticky")) {
-      pageHeader.addClass("is-sticky");
-    }
-  }
-
-  /* *******  make the menu stick on top upon the page scrolling   ********/
-  main_window.on("scroll", function () {
-    if ($(this).scrollTop() > 50) {
-      if (!$(pageHeader).hasClass("is-sticky")) {
-        pageHeader.addClass("is-sticky");
-      }
-    } else {
-      if ($(pageHeader).hasClass("is-sticky")) {
-        pageHeader.removeClass("is-sticky");
-      }
-    }
-  });
-
-  /* *******  show/hide navbar links menu  ********/
-  $(".menu-toggler").on("click", function () {
-    pageHeader.find(".show:not(.menu-wrapper) ").removeClass("show");
-    pageHeader.find(".menu-wrapper").toggleClass("show");
-    $(".menu-toggler").toggleClass("close-menu");
-  });
-
-  /* ******* close the currnt opend menu when click on its wrapper ********/
-  $(".links-list").on("click", function (e) {
-    e.stopPropagation();
-  });
-
-  $(".menu-wrapper").on("click", function () {
-    $(this).removeClass("show");
-    if ($(".menu-toggler").hasClass("close-menu")) {
-      $(".menu-toggler").removeClass("close-menu");
-    }
-  });
-
-  /* ******* showing navbar sub-menus on mobile ********/
-  hasSubMenu.on("click", function (e) {
-    e.stopPropagation();
-    if (!(main_window.innerWidth() > 1199)) {
-      $(this).children(".sub-menu").slideToggle();
-    }
-  });
-
-  /* ******* Start Smooth Scrolling To page sections Area********/
-  $(".landing-page-demo .menu-navbar .menu-link").on("click", function (e) {
-    const link = $(this).attr("href");
-    let currentMainNavHeight = navMain.innerHeight();
-    if (link.charAt(0) === "#") {
-      e.preventDefault();
-      const target = this.hash;
-      $(root).animate(
-        {
-          scrollTop: $(target).offset().top - currentMainNavHeight + 1,
-        },
-        500
-      );
-
-      if (!$(this).parent(".menu-item").hasClass("has-sub-menu")) {
-        // to close the menu after going to crossponding section
-        if ($(".menu-wrapper").hasClass("show")) {
-          $(".menu-wrapper").removeClass("show");
-        }
-
-        // to change the menu toggler buttn icon
-        if ($(".menu-toggler").hasClass("close-menu")) {
-          $(".menu-toggler").removeClass("close-menu");
-        }
-      }
-    }
-  });
-
-
-
-  $(".landing-page-demo .menu-navbar .menu-link").on("click", function (e) {
-    const link = $(this).attr("href");
-    let currentMainNavHeight = navMain.innerHeight();
-    if (link.charAt(0) === "#") {
-      e.preventDefault();
-      const target = this.hash;
-      $(root).animate(
-        {
-          scrollTop: $(target).offset().top - currentMainNavHeight + 1,
-        },
-        500
-      );
-
-      if (!$(this).parent(".menu-item").hasClass("has-sub-menu")) {
-        // to close the menu after going to crossponding section
-        if ($(".menu-wrapper").hasClass("show")) {
-          $(".menu-wrapper").removeClass("show");
-        }
-
-        // to change the menu toggler buttn icon
-        if ($(".menu-toggler").hasClass("close-menu")) {
-          $(".menu-toggler").removeClass("close-menu");
-        }
-      }
-    }
-  });
+});
